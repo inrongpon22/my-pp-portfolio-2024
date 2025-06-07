50c1b2e4079054f1e27bbf85d0d9824f93f13259
inrongpon22 <103518876+inrongpon22@users.noreply.github.com>
2025-06-07 17:07:11 +0700

    update: remove formik, yup --> react-hook-form, zod

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -1,192 +1,75 @@
-"use client";
-import React, { useState } from "react";
-import * as Yup from "yup";
-import clsx from "clsx";
-import { useFormik } from "formik";
-import Link from "next/link";
-import { Button, Card, Divider, Input } from "antd";
-import Image from "next/image";
-import { useRouter } from "next/navigation";
-import { adminServices } from "@/services/AdminServices";
-import { toast } from "react-hot-toast";
-
-const loginSchema = Yup.object().shape({
-  email: Yup.string()
-    .email("Wrong email format")
-    .min(3, "Minimum 3 symbols")
-    .max(50, "Maximum 50 symbols")
-    .required("Email is required"),
-  password: Yup.string()
-    .min(3, "Minimum 3 symbols")
-    .max(50, "Maximum 50 symbols")
-    .required("Password is required"),
-});
+'use client'
+import React, { useState } from 'react'
+import clsx from 'clsx'
+import Link from 'next/link'
+import { Button, Card, Divider, Input } from 'antd'
+import Image from 'next/image'
+import { useRouter } from 'next/navigation'
+import { adminServices } from '@/services/AdminServices'
+import { toast } from 'react-hot-toast'
+import { ArrowLeftOutlined } from '@ant-design/icons'
+import { useForm } from 'react-hook-form'
 
 const initialValues = {
-  email: "admin@demo.com",
-  password: "admin123",
-};
+  email: 'admin@demo.com',
+  password: 'admin123',
+}
 
 const Login = () => {
-  const router = useRouter();
-  const [loading, setLoading] = useState(false);
-  // const {saveAuth, setCurrentUser} = useAuth()
+  const router = useRouter()
+  const [loading, setLoading] = useState<boolean>(false)
 
-  const formik = useFormik({
-    initialValues,
-    validationSchema: loginSchema,
-    onSubmit: async (values, { setStatus, setSubmitting }) => {
-      setLoading(true);
-      try {
-        const res = await adminServices.signInWithEmail(values.email, values.password)
-        console.log('[res]', res)
+  const { register, handleSubmit, formState: { errors } } = useForm()
 
-        localStorage.setItem('admin_token', res.token)
-
-        // await adminServices.signUpWithEmail(values.email, values.password)
-        toast.success("User created successfully")
-      } catch (error) {
-        toast.error("Something went wrong, please try again")
-        console.error(error);
-      } finally {
-        setLoading(false);
-        setSubmitting(false);
-      }
-    },
-  });
+  const onSubmit = (data: any) => {
+    console.log(data)
+  }
 
   return (
-    <form
-      className="form w-100"
-      onSubmit={formik.handleSubmit}
-      noValidate
-    >
-      {/* begin::Heading */}
-      <div className="text-center mb-11">
-        <h1 className="text-black font-bold mb-3">Sign In</h1>
-        <div className="text-gray-500 fw-semibold fs-6">
-          Your Social Campaigns
+    <section className='max-w-4xl mx-auto border rounded-lg p-10'>
+      <Link href='/' className='text-black bg-slate-100 p-1 rounded'>
+        <ArrowLeftOutlined />
+      </Link>
+      <form onSubmit={handleSubmit(onSubmit)} noValidate>
+        {/* begin::Heading */}
+        <div className='text-center mb-11'>
+          <h1 className='text-black font-bold mb-3'>Sign In</h1>
         </div>
-      </div>
-      {/* begin::Heading */}
+        {/* begin::Heading */}
 
-      {/* begin::Login options */}
-      {/* <div className="grid grid-cols-2 gap-4 mb-9">
-        <Button
-          icon={
-            <Image
-              alt="Logo"
-              src="/media/svg/brand-logos/google-icon.svg"
-              width={15}
-              height={15}
-            />
-          }
-        >
-          Sign in with Google
-        </Button>
-
-        <Button
-          icon={
-            <>
-              <Image
-                alt="Logo"
-                src="/media/svg/brand-logos/apple-black.svg"
-                className="theme-light-show"
-                width={15}
-              height={15}
-              />
-            </>
-          }
-        >
-          Sign in with Apple
-        </Button>
-      </div> */}
-      {/* end::Login options */}
-
-      {/* begin::Separator */}
-      {/* <Divider plain>Or with email</Divider> */}
-      {/* end::Separator */}
-
-      {/* {formik.status ? (
-        <div className="mb-lg-15 alert alert-danger">
-          <div className="alert-text font-weight-bold">{formik.status}</div>
+        {/* begin::Form group */}
+        <div className='fv-row mb-3'>
+          <Input
+            placeholder='Email'
+            disabled={loading}
+          />
         </div>
-      ) : (
-        <Card size="small" className="p-3 mb-10">
-          Use account <strong>admin@demo.com</strong> and password{" "}
-          <strong>demo</strong> to continue.
-        </Card>
-      )} */}
+        {/* end::Form group */}
 
-      {/* begin::Form group */}
-      <div className="fv-row mb-3">
-        <Input
-          placeholder="Email"
-          {...formik.getFieldProps("email")}
-          className={clsx("", {
-            error: formik.touched.email && formik.errors.email,
-          })}
-          disabled={loading}
-        />
-        {formik.touched.email && formik.errors.email && (
-          <div className="fv-plugins-message-container">
-            <span role="alert">{formik.errors.email}</span>
-          </div>
-        )}
-      </div>
-      {/* end::Form group */}
+        {/* begin::Form group */}
+        <div className='fv-row mb-3'>
+          <Input
+            type='password'
+            disabled={loading}
+          />
+        </div>
+        {/* end::Form group */}
 
-      {/* begin::Form group */}
-      <div className="fv-row mb-3">
-        <Input
-          type="password"
-          {...formik.getFieldProps("password")}
-          // className={clsx("", {
-          //   error: formik.touched.password && formik.errors.password,
-          // })}
-          disabled={loading}
-        />
-        {formik.touched.password && formik.errors.password && (
-          <div className="fv-plugins-message-container">
-            <div className="fv-help-block">
-              <span role="alert">{formik.errors.password}</span>
-            </div>
-          </div>
-        )}
-      </div>
-      {/* end::Form group */}
+        {/* begin::Action */}
+        <div className='grid mb-10'>
+          <Button
+            // type="primary"
+            id='kt_sign_in_submit'
+            size='large'
+            loading={loading}
+          >
+            {!loading ? 'Sign In' : 'Please wait...'}
+          </Button>
+        </div>
+        {/* end::Action */}
+      </form>
+    </section>
+  )
+}
 
-      {/* begin::Wrapper */}
-      <div className="mb-8 text-end invisible">
-        <Link href="/auth/forgot-password" className="text-blue-500 text-sm">
-          Forgot Password ?
-        </Link>
-      </div>
-      {/* end::Wrapper */}
-
-      {/* begin::Action */}
-      <div className="grid mb-10">
-        <Button
-          // type="primary"
-          id="kt_sign_in_submit"
-          size="large"
-          disabled={formik.isSubmitting || !formik.isValid}
-          onClick={() => formik.handleSubmit()}
-          loading={loading}
-        >
-          {!loading ? "Continue" : "Please wait..."}
-        </Button>
-      </div>
-      {/* end::Action */}
-
-      <div className="text-gray-500 text-center fw-semibold fs-6 invisible">
-        Not a Member yet?{" "}
-        <Link href="/auth/registration" className="text-blue-500">
-          Sign up
-        </Link>
-      </div>
-    </form>
-  );
-};
-
-export default Login;
+export default Login
