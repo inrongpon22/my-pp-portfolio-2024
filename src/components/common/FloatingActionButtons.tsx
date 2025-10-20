16f9a7bebdea15356f28dc759509f7e4cd28cc05
inrongpon22 <103518876+inrongpon22@users.noreply.github.com>
2025-10-20 22:52:54 +0700

    refactor: simplify FloatingActionButtons component and improve theme toggle functionality
    - removed unused button structure and commented out code
    - streamlined the theme toggle button for better accessibility and clarity
    - ensured proper rendering of theme icons based on current theme

--- src/components/common/FloatingActionButtons.tsx
+++ src/components/common/FloatingActionButtons.tsx
@@ -8,16 +8,6 @@ const FloatingActionButtons = () => {
   const { theme, setTheme } = useTheme()
   return (
     <div className="fab">
-      {/* a focusable div with tabIndex is necessary to work on all browsers. role="button" is necessary for accessibility */}
-      <div
-        tabIndex={0}
-        role="button"
-        className="btn btn-lg btn-circle bg-orange-400"
-      >
-        <Plus />
-      </div>
-
-      {/* buttons that show up when FAB is open */}
       <button
         type="button"
         onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
@@ -25,6 +15,23 @@ const FloatingActionButtons = () => {
       >
         {theme === "dark" ? <Sun /> : <Moon  />}
       </button>
+      {/* a focusable div with tabIndex is necessary to work on all browsers. role="button" is necessary for accessibility */}
+      {/* <div
+        tabIndex={0}
+        role="button"
+        className="btn btn-lg btn-circle bg-orange-400"
+      >
+        <Plus />
+      </div> */}
+
+      {/* buttons that show up when FAB is open */}
+      {/* <button
+        type="button"
+        onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
+        className="btn btn-lg btn-circle bg-gray-100 dark:bg-gray-800"
+      >
+        {theme === "dark" ? <Sun /> : <Moon  />}
+      </button> */}
     </div>
   )
 }
