cffd2852a5bb1cf9d0a52fe3962864eb76e324bd
inrongpon22 <103518876+inrongpon22@users.noreply.github.com>
2025-12-01 16:12:12 +0700

    update: theme switcher color

--- src/components/common/FloatingActionButtons.tsx
+++ src/components/common/FloatingActionButtons.tsx
@@ -11,27 +11,10 @@ const FloatingActionButtons = () => {
       <button
         type="button"
         onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
-        className="btn btn-lg btn-circle bg-background"
+        className="btn btn-lg btn-circle bg-foreground"
       >
-        {theme === "dark" ? <Sun /> : <Moon />}
+        {theme === "dark" ? <Sun className="text-background" /> : <Moon className="text-background" />}
       </button>
-      {/* a focusable div with tabIndex is necessary to work on all browsers. role="button" is necessary for accessibility */}
-      {/* <div
-        tabIndex={0}
-        role="button"
-        className="btn btn-lg btn-circle bg-orange-400"
-      >
-        <Plus />
-      </div> */}
-
-      {/* buttons that show up when FAB is open */}
-      {/* <button
-        type="button"
-        onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
-        className="btn btn-lg btn-circle bg-gray-100 dark:bg-gray-800"
-      >
-        {theme === "dark" ? <Sun /> : <Moon  />}
-      </button> */}
     </div>
   )
 }
