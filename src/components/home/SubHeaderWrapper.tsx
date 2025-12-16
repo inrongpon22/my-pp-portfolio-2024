68839b2d40d13ed47f20d4489692a2f2c2109c92
inrongpon22 <103518876+inrongpon22@users.noreply.github.com>
2025-12-16 16:24:39 +0700

    feat: Introduce Terminal component with animated typing and sequence functionality
    - Added Terminal, AnimatedSpan, and TypingAnimation components for enhanced UI interactions
    - Integrated animated typing effects and sequence management for terminal-like output
    - Updated SubHeaderWrapper to include a commented-out example of Terminal usage

--- src/components/home/SubHeaderWrapper.tsx
+++ src/components/home/SubHeaderWrapper.tsx
@@ -1,5 +1,10 @@
 "use client"
 import React from "react"
+import {
+  AnimatedSpan,
+  Terminal,
+  TypingAnimation,
+} from "@/components/ui/terminal"
 
 const SubHeaderWrapper = () => {
   return (
@@ -11,6 +16,46 @@ const SubHeaderWrapper = () => {
           I&apos;m <span className="text-orange-400">Phanupong</span>, Front-End
           Developer.
         </h1>
+        {/* <Terminal>
+          <TypingAnimation>&gt; pnpm dlx shadcn@latest init</TypingAnimation>
+          <AnimatedSpan className="text-green-500">
+            ✔ Preflight checks.
+          </AnimatedSpan>
+          <AnimatedSpan className="text-green-500">
+            ✔ Verifying framework. Found Next.js.
+          </AnimatedSpan>
+          <AnimatedSpan className="text-green-500">
+            ✔ Validating Tailwind CSS.
+          </AnimatedSpan>
+          <AnimatedSpan className="text-green-500">
+            ✔ Validating import alias.
+          </AnimatedSpan>
+          <AnimatedSpan className="text-green-500">
+            ✔ Writing components.json.
+          </AnimatedSpan>
+          <AnimatedSpan className="text-green-500">
+            ✔ Checking registry.
+          </AnimatedSpan>
+          <AnimatedSpan className="text-green-500">
+            ✔ Updating tailwind.config.ts
+          </AnimatedSpan>
+          <AnimatedSpan className="text-green-500">
+            ✔ Updating app/globals.css
+          </AnimatedSpan>
+          <AnimatedSpan className="text-green-500">
+            ✔ Installing dependencies.
+          </AnimatedSpan>
+          <AnimatedSpan className="text-blue-500">
+            <span>ℹ Updated 1 file:</span>
+            <span className="pl-2">- lib/utils.ts</span>
+          </AnimatedSpan>
+          <TypingAnimation className="text-muted-foreground">
+            Success! Project initialization completed.
+          </TypingAnimation>
+          <TypingAnimation className="text-muted-foreground">
+            You may now add components.
+          </TypingAnimation>
+        </Terminal> */}
       </div>
       <h1 className="html-tag sm:ml-4">{`</header>`}</h1>
     </div>
